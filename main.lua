5e93f9c7f9e69a55950cc6e4da71f3ff947ecb47
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-26 20:12:56 +0530

    Init

---
+++ main.lua
@@ -0,0 +1,20 @@
+function love.load()
+    wW, wH = love.graphics.getDimensions()
+    PADDLES = {
+        {
+            ranges = {
+                {10, 10},
+                {10, wH - 10}
+            }
+        }
+    }
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
+
