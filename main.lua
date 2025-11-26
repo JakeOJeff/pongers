f522fd706683f3aafd05d4bbb83884c7698ec866
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-26 20:17:54 +0530

    Setting Keybinds

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ function love.load()
     wW, wH = love.graphics.getDimensions()
     PADDLES = {
         {
+            posKey = "s",
+            negKey = "w",
             ranges = {
                 {10, 10},
                 {10, wH - 10}
@@ -11,7 +13,9 @@ function love.load()
 end
 
 function love.update(dt)
-    
+    for i, v in ipairs(PADDLES) do
+        if love.keyboard.isDown(v.posKey)
+    end
 end
 
 function love.draw()
