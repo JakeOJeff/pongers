1d5d4bbc0847e5af2a96f48fbf4ab336bb003014
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-02 10:57:20 +0530

    Fiixng Depth

--- main.lua
+++ main.lua
@@ -2,10 +2,12 @@ function love.load()
     wW, wH = love.graphics.getDimensions()
     GAME_START = false
     PADDLE_IMG = love.graphics.newImage("paddle.png")
+    PADDLE2_IMG = love.graphics.newImage("paddle2.png")
     BALL_IMG = love.graphics.newImage("ball.png")
+    DEPTH = -15
     BALL = {
-        x = wW/2 - 5,
-        y = wH/2 - 5,
+        x = wW / 2 - 5,
+        y = wH / 2 - 5,
         rad = 20,
         moving = false,
         angle = math.rad(-90),
@@ -23,9 +25,10 @@ function love.load()
             w = 30,
             h = 100,
             ranges = {
-                {10, 10},
-                {10, wH - 10}
+                { 10, 10 },
+                { 10, wH - 10 }
             },
+            img = PADDLE_IMG,
             speed = 200 -- Second Wise
         },
         {
@@ -37,22 +40,35 @@ function love.load()
             w = 30,
             h = 100,
             ranges = {
-                {wW - 10, 10},
-                {wW - 10, wH - 10}
+                { wW - 10, 10 },
+                { wW - 10, wH - 10 }
             },
+            img = PADDLE2_IMG,
             speed = 200 -- Second Wise
         }
+    }
+    PADDLE_EFFECT = {
+        enabled = false,
+        scale = 1,
+        maxScale = 2,
+        hitPaddle = nil
     }
 
     love.graphics.setDefaultFilter("nearest", "nearest")
 end
 
 function love.update(dt)
-
     if #BALL.trail > 50 then
         table.remove(BALL.trail, 1)
         table.remove(BALL.trail, 1)
     end
+    if PADDLE_EFFECT.enabled and PADDLE_EFFECT.scale < PADDLE_EFFECT.maxScale then
+        PADDLE_EFFECT.scale = PADDLE_EFFECT.scale + 5 * dt
+    end
+    if PADDLE_EFFECT.scale >= PADDLE_EFFECT.maxScale then
+        PADDLE_EFFECT.enabled = false
+    end
+
     for i, v in ipairs(PADDLES) do
         if love.keyboard.isDown(v.posKey) then
             if v.ranges[1][1] ~= v.ranges[2][1] then v.x = math.min(v.ranges[2][1] - v.w, v.x + v.speed * dt) end
@@ -81,6 +97,9 @@ function bounceBall()
     if paddleIndex then
         local v = PADDLES[paddleIndex]
         v.score = v.score + 1
+        PADDLE_EFFECT.hitPaddle = v
+        PADDLE_EFFECT.enabled = true
+        PADDLE_EFFECT.scale = 1
 
         BALL.angle = -BALL.angle
 
@@ -92,11 +111,10 @@ function bounceBall()
         end
 
 
-        local relY = (BALL.y - v.y)/v.h 
+        local relY = (BALL.y - v.y) / v.h
         local hitFactor = (relY - 0.5) * math.rad(60)
 
         BALL.angle = BALL.angle + hitFactor
-        
     elseif outOfBounds then
         love.load()
     elseif checkBallBorderCollision() then
@@ -112,12 +130,11 @@ function checkBallCollision()
         local distX = BALL.x - closestX
         local distY = BALL.y - closestY
 
-        local distanceSquared = distX^2 + distY^2
+        local distanceSquared = distX ^ 2 + distY ^ 2
 
         if distanceSquared < BALL.rad * BALL.rad then
             return i
         end
-
     end
     return false
 end
@@ -145,9 +162,22 @@ function love.keypressed(key)
 end
 
 function love.draw()
+    love.graphics.setBackgroundColor(0.45098039215686275, 0.8745098039215686, 0.9490196078431372)
     for i, v in ipairs(PADDLES) do
         -- love.graphics.rectangle("fill", v.x, v.y, v.w, v.h, 10, 10)
-        love.graphics.draw(PADDLE_IMG, v.x, v.y)
+        love.graphics.setColor(0, 0, 0, 0.1)
+        love.graphics.draw(v.img, v.x + DEPTH, v.y)
+        if PADDLE_EFFECT.enabled and PADDLE_EFFECT.hitPaddle == v then
+            love.graphics.setColor(1, 1, 1, 1 - (PADDLE_EFFECT.scale / PADDLE_EFFECT.maxScale))
+            local ox, oy = v.x + v.w/2, v.y + v.h/2
+            love.graphics.push()
+            love.graphics.translate(ox, oy)
+            love.graphics.scale(PADDLE_EFFECT.scale, PADDLE_EFFECT.scale)
+            love.graphics.draw(v.img, -v.w/2, -v.h/2)
+            love.graphics.pop()
+        end
+        love.graphics.setColor(1, 1, 1, 1)
+        love.graphics.draw(v.img, v.x, v.y)
         love.graphics.print(v.score, v.x - 5, v.y - 5)
     end
     if #BALL.trail >= 4 then
@@ -156,7 +186,9 @@ function love.draw()
     -- love.graphics.circle("fill", BALL.x, BALL.y, BALL.rad)
     love.graphics.push()
     love.graphics.translate(-BALL.rad, -BALL.rad)
+    love.graphics.setColor(0, 0, 0, 0.1)
+    love.graphics.draw(BALL_IMG, BALL.x + DEPTH, BALL.y)
+    love.graphics.setColor(1, 1, 1, 1)
     love.graphics.draw(BALL_IMG, BALL.x, BALL.y)
     love.graphics.pop()
 end
-
