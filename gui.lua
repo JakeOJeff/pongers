6673704746a35b87c8a1719812a1c258ae40621a
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-26 19:54:41 +0530

    Fix GUI

--- gui.lua
+++ gui.lua
@@ -12,12 +12,19 @@ local gui = {
 }
 
 function gui:update()
-
+    local mx, my = love.mouse.getPosition()
+    if mx >= self.button.x - self.button.w / 2 and mx <= self.button.x + self.button.w / 2 and
+        my >= self.button.y - self.button.h - 32 and my <= self.button.y - 32 then
+        self.button.hovering = true
+    else
+        self.button.hovering = false
+    end
 end
 
 function gui:mousepressed(x, y, button)
     if x >= self.button.x - self.button.w / 2 and x <= self.button.x + self.button.w / 2 and
         y >= self.button.y - self.button.h - 32 and y <= self.button.y - 32 then
+        self.button.hovering = true
         if button == 1 then
             if STATE == "AI" then
                 STATE = "AI GUIDE"
@@ -41,12 +48,13 @@ function gui:draw()
         love.graphics.setColor(1, 1, 1)
         love.graphics.print(text, wW / 2 - FONT:getWidth(text) / 2, wH / 2 - FONT:getHeight() / 2)
     end
+    local offset = self.button.hovering and 5 or 0
     love.graphics.setColor(self.button.colorDark)
     love.graphics.rectangle("fill", self.button.x - self.button.w / 2, self.button.y - self.button.h - 32, self.button.w,
         self.button.h, 2, 2, 2)
     love.graphics.setColor(self.button.color)
-    love.graphics.rectangle("fill", self.button.x - self.button.w / 2 + 2, self.button.y - self.button.h - 30,
-        self.button.w - 4, self.button.h - 4, 2, 2, 2)
+    love.graphics.rectangle("fill", self.button.x - self.button.w / 2 + 2 + offset, self.button.y - self.button.h - 30 + offset,
+        self.button.w - 4 - offset * 2, self.button.h - 4 - offset * 2, 2, 2, 2)
     love.graphics.setColor(self.button.colorDark)
     love.graphics.print(self.button.state, self.button.x - FONT:getWidth(self.button.state) / 2,
         self.button.y - self.button.h / 2 - 32 - FONT:getHeight() / 2)
