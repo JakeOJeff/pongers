2d698650d3449198220cfa5222fb5c6470232302
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-12-31 12:17:46 +0530

    Added AUDIO systems

---
+++ audio.lua
@@ -0,0 +1,10 @@
+local audio = {
+    paddle = love.graphics.newAudio("audio/hit.mp3"),
+    death = love.graphics.newAudio("audio/death.mp3"),
+    bgm = love.graphics.newAudio("audio/bgm.mp3")
+
+}
+
+
+
+return audio
